927980bbc47d78877879c71dc3d3689d711b030b
Joe Mafrici <hello@joemafrici.com>
2023-02-28 15:00:20 -0800

    Insertion Sort implemented and tested

--- Algorithms/sorts.c
+++ Algorithms/sorts.c
@@ -2,7 +2,7 @@
 // Purpose: Implementation of the Insertion Sort algorithm
 
 //***********************************************
-// Sorts an array of integers
+// Sorts an array of positive integers in ascending order
 // array: pointer to array of integers
 // size: size of the array
 // example input: array = {5, 2, 4, 6, 1, 3}
@@ -14,7 +14,7 @@ void insertion_sort(int *array, int size)
         int key = array[jj];
         // insert key into sorted sequence
         int ii = jj - 1;
-        while (ii > 0 && array[ii] > key)
+        while (ii >= 0 && array[ii] > key)
         {
             array[ii + 1] = array[ii];
             ii = ii - 1;
--- Algorithms/test_runner.c
+++ Algorithms/test_runner.c
@@ -7,40 +7,109 @@
 #include "sorts.h"
 
 static int g_size;
-static char *helpstring = "test_runner\n"
+static int g_print_level;
+static char *helpstring = "tester\n"
                                 "-h print help string\n"
-                                "-s <size> size of array to sort";
+                                "-s <size> size of array to sort\n"
+                                "-v verbose mode: print array contents\n";
 void process_args(int argc, char **argv);
+int validate_sort(int *data, int size);
 //***********************************************
 int main(int argc, char **argv)
 {
     g_size = 200;
+    g_print_level = 0;
     process_args(argc, argv);
     srand((unsigned int)time(NULL));
     int data[g_size];
+
+    printf("filling array with %d positive integers\n", g_size);
+
     for (int ii = 0; ii < g_size; ++ii)
     {
         data[ii] = rand() % 1000;
     }
+
+    if (g_print_level)
+    {
+        printf("writing array contents to beforesort.txt...\n");
+        FILE *file = fopen("beforesort.txt", "w+");
+        for (int ii = 0; ii < g_size; ++ii)
+        {
+            fprintf(file, "%d\n", data[ii]);
+        }
+        if (fclose(file) != 0) printf("error closing file\n");
+    }
+
+    printf("sorting...\n");
+
     insertion_sort(data, g_size);
+
+    printf("verifying sort...\n");
+
+    int sort_valid = validate_sort(data, g_size);
+
+    if (sort_valid) printf("test successful... sort is valid!\n");
+    else printf("test failed...\n");
+
+    if (g_print_level)
+    {
+        printf("array contents written to aftersort.txt\n");
+        FILE *file = fopen("aftersort.txt", "w+");
+        for (int ii = 0; ii < g_size; ++ii)
+        {
+            fprintf(file, "%d\n", data[ii]);
+        }
+        if (fclose(file) != 0) printf("error closing file\n");
+    }
+
+
+
+}
+
+//***********************************************
+// Verify if array is sorted in ascending order
+// data: array to verify
+// size: size of array
+// return: 1 if array is sorted, 0 if not sorted
+int validate_sort(int *data, int size)
+{
+    int prev = 0;
+    for (int ii = 0; ii < g_size; ++ii)
+    {
+        if (prev > data[ii])
+        {
+           return 0;
+        }
+    }
+    return 1;
 }
 //***********************************************
 // Process command line arguments
 void process_args(int argc, char **argv)
 {
     int c;
-    while ((c = getopt(argc, argv, "hs:")) != -1)
+    if (argc == 1)
+    {
+        printf("%s\n", helpstring);
+    }
+    while ((c = getopt(argc, argv, "hs:v")) != -1)
     {
         switch(c)
         {
             case 'h':
                 printf("%s\n", helpstring);
+                exit(0);
                 break;
             case 's':
                 g_size = atoi(optarg);
                 break;
-            default:
+            case 'v':
+                g_print_level = 1;
+                break;
+            case '?':
                 printf("%s\n", helpstring);
+                exit(0);
                 break;
         }
     }

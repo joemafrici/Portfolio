a5f5c731fb2a94661fb7b2e5222b6f28905a9583
Joe Mafrici <hello@joemafrici.com>
2023-03-02 12:29:59 -0800

    add declaration for merge sort

--- Algorithms/sorts.c
+++ Algorithms/sorts.c
@@ -9,7 +9,7 @@
 // size: size of the array
 // example input: array = {5, 2, 4, 6, 1, 3}
 // example output: array = {1, 2, 3, 4, 5, 6}
-void insertion_sort(int *array, int size)
+void insertion_sort(int *array, size_t size)
 {
     for (size_t jj = 1; jj < size; ++jj)
     {
@@ -23,4 +23,16 @@ void insertion_sort(int *array, int size)
         }
         array[ii + 1] = key;
     }
+}
+
+//***********************************************
+// Sorts an array of doubles in ascending order
+// array: pointer to an array of doubles
+// size: size of the array
+// example input: array = {5.8, 2.1, 5.5, 3.2, 1.0, 3.1}
+// example output: array = {1.0, 2.1, 3.1, 3.2, 5.5, 5.8}
+void merge_sort(double *array, size_t size)
+{
+    // recursively split the array into subarrays until the
+    // subarray size is 1
 }
--- Algorithms/sorts.h
+++ Algorithms/sorts.h
@@ -1,1 +1,3 @@
-void insertion_sort(int *array, int size);
+void insertion_sort(int *array, size_t size);
+
+void merge_sort(double *array, size_t size);
